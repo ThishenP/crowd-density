fixed training, normalisation and gpu

dataset.py
@@ -6,35 +6,44 @@ import numpy as np
 import random
 import cv2
 
+####
+from torchvision import transforms
+
 class CDEDataset(Dataset):
-    def __init__(self,im_list,root_X,root_y, transform = None):
+    def __init__(self,im_list,root_X,root_y, transform = None, train = True):
         self.root_X = root_X
         self.root_y = root_y
         self.im_list= im_list
         self.transform = transform
+        self.train = train
     
     def __len__(self):
         return len(self.root_X)#check if right len
     
     def __getitem__(self,index):
-        image,gt = read_data(self.im_list[index], self.root_X, self.root_y)
-        
+        image,gt = read_data(self.im_list[index], self.root_X, self.root_y, train = self.train)
+
+        tr = transforms.ToTensor()
+        gt = tr(gt)
         if self.transform:
             image = self.transform(image)
-            gt = self.transform(gt)
+        else:
+            image = tr(image)
         
         return image.float(), gt.float()
-        #return image.float(), gt.float()
 
 def read_data(image_name,root_X, root_GT, train = True):
     image_path = root_X + "/" + image_name
-    image = Image.open(image_path).convert('RGB')
-    print(image)
+    
+    image = Image.open(image_path)
+
+    image = image.convert('RGB')
+
     gt_path = root_GT + f"/{image_name[:-4]}.h5"
     
     gt = np.asarray(h5py.File(gt_path, 'r')['density'])
 
-    if train:
+    if False:
         ratio = 0.5
         crop_size = (int(image.size[0]*ratio),int(image.size[1]*ratio))
         rdn_value = random.random()
@@ -56,8 +65,7 @@ def read_data(image_name,root_X, root_GT, train = True):
         if random.random()>0.8:
             gt = np.fliplr(gt)
             image = image.transpose(Image.FLIP_LEFT_RIGHT)
-    #print(gt.shape[1]/8)
-    #target = cv2.resize(gt,(int(gt.shape[1]/8),int(gt.shape[0]/8)),interpolation = cv2.INTER_CUBIC)*64
+    
     target = cv2.resize(gt,(gt.shape[1]//8,gt.shape[0]//8),interpolation = cv2.INTER_CUBIC)*64
 
     return image, target

train.py
@@ -1,78 +1,75 @@
-from numpy.core.fromnumeric import resize
 from model import Net
 from dataset import CDEDataset
 from torchvision import transforms
-
-from PIL import Image
 import torch
 import os
-import cv2
+import matplotlib.pyplot as plt
 
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
 
+
 def main():
+
+    if torch.cuda.is_available(): 
+        device = torch.device("cuda:0")
+    else:
+        device = torch.device("cpu")
+    device
+
+
     net = Net([(512,2),(512,2),(512,2),(256,2), (128,2), (64,2)], 23, 512)
+
     root_ims = 'ShanghaiTech/ShanghaiTech/part_A/train_data/images'
     root_ann = 'ShanghaiTech/ShanghaiTech/part_A/train_data/density_gt'
     im_list = os.listdir(root_ims)
     train = CDEDataset(im_list,root_ims,root_ann, transform = transforms.Compose([
-                transforms.ToTensor(),
-                #transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-            ]))
+                    transforms.ToTensor(),
+                    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+                ]))
 
     root_ims = 'ShanghaiTech/ShanghaiTech/part_A/test_data/images'
     root_ann = 'ShanghaiTech/ShanghaiTech/part_A/test_data/density_gt'
     im_list = os.listdir(root_ims)
     test = CDEDataset(im_list,root_ims,root_ann, transform  = transforms.Compose([
-                transforms.ToTensor(),
-                #transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-            ]))
+                    transforms.ToTensor(),
+                    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+                ]))
 
-    
-
-    train_dataloader = DataLoader(train, batch_size=1, shuffle=True) #batch size of 1 since all different sizes
+    train_dataloader = DataLoader(train, batch_size=1, shuffle=True)#batch size of 1 since all different sizes
     test_dataloader = DataLoader(test, batch_size=1, shuffle=True)
 
-    criterion = nn.MSELoss(reduction='sum')# same as nn.MSELoss(size_average=False)
+    criterion = nn.MSELoss(reduction='sum').to(device)# same as nn.MSELoss(size_average=False)
     optimizer = optim.SGD(net.parameters(), lr=1e-7, momentum=0.9)
-    
-    for epoch in range(3):  # loop over the dataset multiple times
-        print(epoch)
+
+    num_epochs = 10
+
+    for epoch in range(num_epochs):  # loop over the dataset multiple times
+
         running_loss = 0.0
         for i, data in enumerate(train_dataloader, 0):
             # get the inputs; data is a list of [inputs, labels]
             inputs, labels = data
+
+            inputs = inputs.to(device)
+            labels = labels.to(device)
             # zero the parameter gradients
             optimizer.zero_grad()
 
             # forward + backward + optimize
             outputs = net(inputs)
-            print(outputs.shape)
-            #quick fix idk if right:
-            # ***********use this to preprocess - resizing is slow
-            # print(labels.shape)
-            # print(torch.sum(labels))
-            # divisor = [labels.shape[2]/outputs.shape[2],labels.shape[3]/outputs.shape[3]]
-            # resize_transform = transforms.Resize(size = (outputs.shape[2],outputs.shape[3]),interpolation = Image.BICUBIC)
-            # labels = resize_transform(labels) #* divisor[0]*divisor[1]
-            # print(torch.sum(labels))
-            # print(labels.shape)
 
             loss = criterion(outputs,labels)
             loss.backward()
             optimizer.step()
-            print(loss)
-            print(loss.item())
+
             # print statistics
             running_loss += loss.item()
-            if i % 4 == 0:    # print every 2000 mini-batches
-                print('[%d, %5d] loss: %.3f' %
-                    (epoch + 1, i + 1, running_loss / 4))
-                running_loss = 0.0
+        print(running_loss/300)
 
     print('Finished Training')
+
 
 if __name__ == '__main__':
     main()