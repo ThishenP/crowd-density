69c55d3a427ccbee135e5e6e125a2d4b6ec6c395
ThishenP <60894050+ThishenP@users.noreply.github.com>
2021-11-05 21:31:27 +0200

    train all 3

--- model.py
+++ model.py
@@ -2,21 +2,39 @@ import torch.nn as nn
 import torch
 from torchvision import models, transforms
 
-class Net(nn.Module):
-    def __init__(self, arc, num_vgg_layers, num_channels_after_vgg, aspp = False):
+class BaseNet(nn.Module):
+    def __init__(self, arc, num_vgg_layers, num_channels_after_vgg):
         super().__init__()
-        self.aspp = aspp
+
+        vgg16 = models.vgg16_bn(pretrained=True)
+        for param in vgg16.features.parameters():
+            param.require_grad = False
+        self.vgg_layers = vgg16.features[:num_vgg_layers]
+        self.dilated_layers = create_layers(arc, num_channels_after_vgg) #in size may change depending on number of vgg layers
+        self.last = nn.Conv2d(arc[-1][0], 1, kernel_size=1)
+        
+                
+    def forward(self, image):
+        vgg_out = self.vgg_layers(image)
+        out = self.dilated_layers(vgg_out)
+        out = self.last(out)
+
+        return out
+
+class ASPPNet(nn.Module):
+    def __init__(self, arc, num_vgg_layers, num_channels_after_vgg):
+        super().__init__()
 
         vgg16 = models.vgg16(pretrained=True)
         for param in vgg16.features.parameters():
             param.require_grad = False
         self.vgg_layers = vgg16.features[:num_vgg_layers]
 
-        if self.aspp:
-            self.aspp1 = nn.Conv2d(num_channels_after_vgg, int(arc[0][0]/4), kernel_size=3, padding= 2 ,dilation = 2)
-            self.aspp2 = nn.Conv2d(num_channels_after_vgg, int(arc[0][0]/4), kernel_size=3, padding= 4 ,dilation = 4)
-            self.aspp3 = nn.Conv2d(num_channels_after_vgg, int(arc[0][0]/4), kernel_size=3, padding= 8 ,dilation = 8)
-            self.aspp4 = nn.Conv2d(num_channels_after_vgg, int(arc[0][0]/4), kernel_size=3, padding= 16 ,dilation = 16)
+        
+        self.aspp1 = nn.Conv2d(num_channels_after_vgg, int(arc[0][0]/4), kernel_size=3, padding= 6 ,dilation = 6)
+        self.aspp2 = nn.Conv2d(num_channels_after_vgg, int(arc[0][0]/4), kernel_size=3, padding= 12 ,dilation = 12)
+        self.aspp3 = nn.Conv2d(num_channels_after_vgg, int(arc[0][0]/4), kernel_size=3, padding= 18 ,dilation = 18)
+        self.aspp4 = nn.Conv2d(num_channels_after_vgg, int(arc[0][0]/4), kernel_size=3, padding= 24 ,dilation = 24)
 
         self.dilated_layers = create_layers(arc, num_channels_after_vgg) #in size may change depending on number of vgg layers
         self.last = nn.Conv2d(arc[-1][0], 1, kernel_size=1)
@@ -24,20 +42,64 @@ class Net(nn.Module):
                 
     def forward(self, image):
         vgg_out = self.vgg_layers(image)
-        if self.aspp:
-            x1 = self.aspp1(vgg_out)
-            x2 = self.aspp2(vgg_out)
-            x3 = self.aspp3(vgg_out)
-            x4 = self.aspp4(vgg_out)
-            out = torch.cat((x1, x2, x3, x4), dim=1)
-        else:
-            out = vgg_out
         
-        
+        x1 = self.aspp1(vgg_out)
+        x2 = self.aspp2(vgg_out)
+        x3 = self.aspp3(vgg_out)
+        x4 = self.aspp4(vgg_out)
+        out = torch.cat((x1, x2, x3, x4), dim=1)
+
         out = self.dilated_layers(out)
         out = self.last(out)
 
+        return out
 
+class SkipASPPNet(nn.Module):
+    def __init__(self, num_vgg_layers, num_channels_after_vgg):
+        super().__init__()
+
+
+        vgg16 = models.vgg16(pretrained=True)
+
+        for param in vgg16.features.parameters():
+            param.require_grad = False
+        self.vgg_layers = vgg16.features[:num_vgg_layers]
+
+        
+        self.aspp1 = nn.Conv2d(num_channels_after_vgg, 64, kernel_size=3, padding= 6 ,dilation = 6)
+        self.aspp2 = nn.Conv2d(num_channels_after_vgg, 64, kernel_size=3, padding= 12 ,dilation = 12)
+        self.aspp3 = nn.Conv2d(num_channels_after_vgg, 64, kernel_size=3, padding= 18 ,dilation = 18)
+        self.aspp4 = nn.Conv2d(num_channels_after_vgg, 64, kernel_size=3, padding= 24 ,dilation = 24)
+
+        self.dilated1 = nn.Conv2d(256,256,kernel_size = 3, padding =2, dilation = 2)
+        self.dilated2 = nn.Conv2d(256,128,kernel_size = 3, padding =2, dilation = 2)
+        self.dilated3 = nn.Conv2d(128+256,128,kernel_size = 3, padding =2, dilation = 2)
+        self.dilated4 = nn.Conv2d(128,128,kernel_size = 3, padding = 2, dilation = 2)
+        self.dilated5 = nn.Conv2d(128+128,64,kernel_size = 3, padding = 2, dilation = 2)
+        self.dilated6 = nn.Conv2d(64,64,kernel_size = 3, padding = 2, dilation = 2)
+
+       # self.dilated_layers = create_layers(arc, num_channels_after_vgg) #in size may change depending on number of vgg layers
+        self.last = nn.Conv2d(64, 1, kernel_size=1)
+        
+                
+    def forward(self, image):
+        #print(image.shape)
+        vgg_out = self.vgg_layers(image)
+        #print('vgg:',vgg_out.shape)
+        x1 = self.aspp1(vgg_out)
+        x2 = self.aspp2(vgg_out)
+        x3 = self.aspp3(vgg_out)
+        x4 = self.aspp4(vgg_out)
+        cat = torch.cat((x1, x2, x3, x4), dim=1)
+
+        out = self.dilated1(cat)
+        skip = self.dilated2(out)
+        out = self.dilated3(torch.cat((skip,cat), dim=1))
+        out = self.dilated4(out)
+        out = self.dilated5(torch.cat((out,skip), dim=1))
+        out = self.dilated6(out)
+        
+        out = self.last(out)
 
         return out
 
--- train.py
+++ train.py
@@ -1,11 +1,11 @@
-from model import Net
+from model import BaseNet, ASPPNet, SkipASPPNet
 from dataset import CDEDataset
 from torchvision import transforms
 import torch
 import os
 import matplotlib.pyplot as plt
 import argparse
-
+import numpy as np
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
@@ -23,30 +23,35 @@ def parse_args():
     return args
 def main():
     args = parse_args()
-    wb = False ###############
-    shut_down = False
-    aspp = False
-    save = False
-    hyper_params = [""]
-
-    if wb:
-        wandb.init(project="cde", entity="thishen")
-        config = wandb.config
-        config.learning_rate = 1e-7
-
     if torch.cuda.is_available(): 
         device = torch.device("cuda:0")
     else:
         device = torch.device("cpu")
-    device
 
+    net = BaseNet([(256,2),(256,2), (128,2),(128,2), (64,2), (64,2)], 16, 256).to(device)
+    train(net, 'base', lr = 2e-5, batch_size=8, epochs = 1000,save = True)    
 
-    net = Net([(256,2),(256,2), (128,2),(128,2), (64,2), (64,2)], 16, 256, aspp=aspp).to(device)
+    net = ASPPNet([(256,2),(256,2), (128,2),(128,2), (64,2), (64,2)], 16, 256).to(device)
+    train(net, 'aspp', lr = 2e-5, batch_size=8, epochs = 1000,save = True)
+
+    net = SkipASPPNet(16, 256).to(device)
+    train(net, 'skip-aspp', lr = 1e-6, batch_size=6, epochs = 1000,save = True)
+
+def train(net, model_name, lr=1e-6, batch_size=6, epochs = 800, wb = False, shut_down = False, save = False):
+    print(f"training {model_name}")
+    if torch.cuda.is_available(): 
+        device = torch.device("cuda:0")
+    else:
+        device = torch.device("cpu")
+
+    if wb:
+        wandb.init(project="cde", entity="thishen")
+        config = wandb.config
 
     root_ims = '../CDE_Data/train/images'
     root_ann = '../CDE_Data/train/density_gt'
-    im_list = os.listdir(root_ims)
 
+    im_list = os.listdir(root_ims)
     
     train = CDEDataset(im_list,root_ims,root_ann, transform = transforms.Compose([
                     transforms.ToTensor(),
@@ -55,6 +60,7 @@ def main():
 
     root_ims = '../CDE_Data/val/images'
     root_ann = '../CDE_Data/val/density_gt'
+
     im_list = os.listdir(root_ims)
 
     val = CDEDataset(im_list,root_ims,root_ann, transform = transforms.Compose([
@@ -64,22 +70,22 @@ def main():
 
 
 
-    train_dataloader = DataLoader(train, batch_size=6, shuffle=True)#batch size of 1 since all different sizes
+    train_dataloader = DataLoader(train, batch_size=batch_size, shuffle=True)#batch size of 1 since all different sizes
     val_dataloader = DataLoader(val, batch_size=1, shuffle=False)
 
 
     criterion = nn.MSELoss(reduction='sum').to(device)# same as nn.MSELoss(size_average=False)
     
-    optimizer = optim.SGD(net.parameters(), lr=1e-5, momentum=0.9) #lr = 1e-7
+    #optimizer = optim.SGD(net.parameters(), lr=lr, momentum=0.9) #lr = 1e-7
+    optimizer = optim.Adam(net.parameters(), lr=lr) # lr=1e-6 for skip
 
-    num_epochs = 800
+    num_epochs = epochs
 
     if save:
         now = datetime.now()
-        
         run_start_datetime = now.strftime("%d-%m-%Y_%H-%M-%S")
-        os.mkdir(f'models/{run_start_datetime}')
-        os.mkdir(f'../checkpoints/{run_start_datetime}')
+        os.mkdir(f'models/{run_start_datetime}-{model_name}')
+        os.mkdir(f'../checkpoints/{run_start_datetime}-{model_name}')
 
     for epoch in range(num_epochs):  # loop over the dataset multiple times
 
@@ -101,14 +107,34 @@ def main():
             loss.backward()
             optimizer.step()
 
-            # print statistics
             running_loss += loss.item()
 
+        epoch_loss = running_loss/((int(len(train)/batch_size))+1)
+        print(epoch_loss)
 
-        print(running_loss/48)
         if wb:
-            wandb.log({"loss": running_loss/48})
+            wandb.log({"loss": epoch_loss})
+
+
         if epoch%50 == 0: 
+            val_losses = []
+            with torch.no_grad():    
+                for i, data in enumerate(val_dataloader, 0):
+                    # evaluate the model on the test set
+                    inputs_val, labels_val = data
+
+                    inputs_val = inputs_val.to(device)
+                    labels_val = labels_val.to(device)
+                    outputs_val = net(inputs_val)
+                    
+                    pred_count = np.sum(outputs_val[0][0].cpu().detach().numpy())
+                    act_count = np.sum(labels_val[0].cpu().detach().numpy())
+                    
+                    val_losses.append(pred_count - act_count)
+
+                mae = np.mean(np.abs(val_losses))
+                if wb:
+                    wandb.log({"mae-on-validation": mae})
             if save:
                 torch.save({
                 'epoch': epoch,
@@ -116,8 +142,8 @@ def main():
                 'optimizer_state_dict': optimizer.state_dict(),
                 'loss': loss,
 
-                }, f"../checkpoints/{run_start_datetime}/checkpoint_at_{epoch}.pt")
-                torch.save(net, f"models/{run_start_datetime}/model_at_{epoch}.pt")
+                }, f"../checkpoints/{run_start_datetime}-{model_name}/checkpoint_at_{epoch}.pt")
+                torch.save(net, f"models/{run_start_datetime}-{model_name}/model_at_{epoch}.pt")
     if save:
         torch.save({
                 'epoch': epoch,
@@ -125,8 +151,8 @@ def main():
                 'optimizer_state_dict': optimizer.state_dict(),
                 'loss': loss,
 
-                }, f"../checkpoints/{run_start_datetime}/checkpoint_at_{epoch}.pt")
-        torch.save(net, f"models/{run_start_datetime}/model_at_{num_epochs}.pt")    
+                }, f"../checkpoints/{run_start_datetime}-{model_name}/checkpoint_at_{epoch}.pt")
+        torch.save(net, f"models/{run_start_datetime}-{model_name}/model_at_{num_epochs}.pt")    
     print('Finished Training')
     
     if shut_down:
