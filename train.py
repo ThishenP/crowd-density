e67706c3aa36fd1dc098e61407ac271be7fd9073
Thishen Packirisamy <60894050+ThishenP@users.noreply.github.com>
2021-11-10 01:00:02 +0200

    Update train.py

--- train.py
+++ train.py
@@ -27,15 +27,16 @@ def main():
         device = torch.device("cuda:0")
     else:
         device = torch.device("cpu")
+    
+    net = SkipASPPNet(16, 256).to(device)
+    train(net, 'skip-aspp', lr = 1e-6, batch_size=6, epochs = 2000,save = True, wb = True)
 
     net = BaseNet([(256,2),(256,2), (128,2),(128,2), (64,2), (64,2)], 16, 256).to(device)
-    train(net, 'base', lr = 2e-5, batch_size=8, epochs = 1000,save = True, wb = True)    
+    train(net, 'base', lr = 2e-5, batch_size=8, epochs = 1200,save = True, wb = True)    
 
     net = ASPPNet([(256,2),(256,2), (128,2),(128,2), (64,2), (64,2)], 16, 256).to(device)
-    train(net, 'aspp', lr = 2e-5, batch_size=8, epochs = 1000,save = True, wb = True)
+    train(net, 'aspp', lr = 2e-5, batch_size=8, epochs = 1200,save = True, wb = True)
 
-    net = SkipASPPNet(16, 256).to(device)
-    train(net, 'skip-aspp', lr = 1e-6, batch_size=6, epochs = 1000,save = True, wb = True)
 
 def train(net, model_name, lr=1e-6, batch_size=6, epochs = 800, wb = False, shut_down = False, save = False):
     print(f"training {model_name}")
